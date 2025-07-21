ee2a6fb1b829cce9097dc7c991d8495630273f4d
Cshraddha1503 <shraddha76830@gmail.com>
2025-07-22 03:04:12 +0530

    Project Completed

---
+++ cmd/Students-API/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/Cshraddha153/Students-API/internal/http/handlers/student"
+	"github.com/Cshraddha153/Students-API/internal/config"
+	"github.com/Cshraddha153/Students-API/internal/storage/sqlite"
+
+)
+
+func main() {
+	// load config
+	cfg := config.MustLoad()
+
+	// database setup ok
+	storage, err := sqlite.New(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	slog.Info("storage initialized", slog.String("env", cfg.Env), slog.String("version", "1.0.0"))
+
+
+	// setup router
+	router := http.NewServeMux()
+
+	router.HandleFunc("POST /api/students", student.New(storage))
+	router.HandleFunc("GET /api/students/{id}", student.GetById(storage))
+	router.HandleFunc("GET /api/students", student.GetList(storage))
+
+	// setup server
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+
+	slog.Info("Server started", slog.String("address", cfg.Addr))
+	fmt.Printf("Server Started %s", cfg.Addr)
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal("failed to start server")
+		}
+	}()
+
+	<-done
+
+	slog.Info("Shutting down the server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("failed to shutdown server", slog.String("error", err.Error()))
+	}
+
+	slog.Info("Server shutdown successfully")
+
+}
