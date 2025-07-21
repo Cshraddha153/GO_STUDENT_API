ac28a06f444131ebbb865f49f58652e5a8ef88e1
Cshraddha1503 <shraddha76830@gmail.com>
2025-07-22 02:03:36 +0530

    add get sudents

--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -6,13 +6,15 @@ import(
 	"errors"
 	"github.com/go-playground/validator/v10"
 	"github.com/Cshraddha153/Students-API/internal/types"
+	"github.com/Cshraddha153/Students-API/internal/storage"
 	"github.com/Cshraddha153/Students-API/internal/utils/response"
 	"io"
-	// "log/slog"
+	"log/slog"
 	"net/http"
+	"strconv"
 )
 
-func New() http.HandlerFunc {
+func New(storage storage.Storage) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request){
 
 		var student types.Student
@@ -36,9 +38,45 @@ func New() http.HandlerFunc {
 			response.WriteJson(w, http.StatusBadRequest, response.ValidationError(validateErrs))
 			return
 		}
+
+		lastId, err := storage.CreateStudent(
+			student.Name, 
+			student.Email, 
+			student.Age,
+		)
+
+		slog.Info("user created successfully", slog.String("userId", fmt.Sprint(lastId)))
+
+		if err != nil {
+			response.WriteJson(w, http.StatusInternalServerError, err)
+			return	
+		}
 		
 		
 		// w.Write([]byte("Welcome to students api"))
-		response.WriteJson(w, http.StatusCreated, map[string]string {"success": "ok"})
+		response.WriteJson(w, http.StatusCreated, map[string]int64{"id": lastId})
+	}
+}
+
+func GetById(storage storage.Storage) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request){
+		id := r.PathValue("id")
+		slog.Info("getting a student", slog.String("id", id))
+
+		intId, err := strconv.ParseInt(id, 10, 64)
+		if err != nil {
+			response.WriteJson(w, http.StatusBadRequest, response.GeneralError(err))
+			return
+		}
+
+		students, err := storage.GetStudentById(intId)
+
+		if err != nil{
+			slog.Error("error getting user", slog.String("id", id))
+			response.WriteJson(w, http.StatusInternalServerError, response.GeneralError(err))
+			return
+		}
+
+		response.WriteJson(w, http.StatusOK, students)
 	}
 }
