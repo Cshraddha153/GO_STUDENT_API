9d350ea96982f3a8b06f59a7b272fe62e8600fbd
Cshraddha1503 <shraddha76830@gmail.com>
2025-07-22 02:33:15 +0530

    project completed

--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -80,3 +80,20 @@ func GetById(storage storage.Storage) http.HandlerFunc {
 		response.WriteJson(w, http.StatusOK, students)
 	}
 }
+
+
+func GetList(storage storage.Storage) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request){
+
+		slog.Info("getting all student")
+
+		students, err := storage.GetStudents()
+		if err != nil {
+			response.WriteJson(w, http.StatusInternalServerError, err)
+			return
+		}
+
+		response.WriteJson(w, http.StatusOK, students)
+		
+	}
+}
